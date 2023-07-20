73b599131918987d8ef31876edf8db6daafea046
armeljunior <armeljr@gmail.com>
2023-07-20 23:52:53 +0100

    finished_1

--- Project1.py
+++ Project1.py
@@ -1,1 +1,10 @@
-print("Hello World")
+print("Welcome to the band name Generator.\n")
+
+userInputCity = input("What City did you grow up in ? \n")
+
+userInputPet = input("Give me a name of a pet. \n")
+
+FinalBandName = userInputCity + userInputPet
+
+
+print("Your name could be :" + userInputCity + " "+ userInputPet)
