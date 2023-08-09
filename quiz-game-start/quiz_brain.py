82d63984236b3b207f72e204a90710eeb1440169
armeljr <armeljr@gmail.com>
2023-08-09 16:47:47 +0100

    Update quiz_brain.py

--- quiz-game-start/quiz_brain.py
+++ quiz-game-start/quiz_brain.py
@@ -1,14 +1,9 @@
-import main
-import question_model
-from main import question_bank
-import random
-
 class QuizBrain:
     def __init__(self,q_list):
         self.question_number = 0
-        self.questionlist = q_list
+        self.question_list = q_list
 
     def next_question(self):
         current_question = self.question_list[self.question_number]
-        useranswer = input(f"Q,{}: {current_question.text} (true/false)" ) 
+        input(f"Q.{self.question_number}: {current_question.text}(true/false): ") 
         
